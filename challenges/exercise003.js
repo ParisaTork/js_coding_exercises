586e0b3ac7dfa0bb1919c7b3a33db2e0a941d162
ParisaTork <47482049+ParisaTork@users.noreply.github.com>
2022-02-17 10:45:36 +0000

    Post-feedback refactoring - Exercise 003

--- challenges/exercise003.js
+++ challenges/exercise003.js
@@ -1,21 +1,22 @@
+/* eslint-disable max-len */
 function getSquares(nums) {
   if (nums === undefined) throw new Error('nums is required');
   return nums.map((num) => Math.pow(num, 2));
 }
 
-// need to refactor
+// refactored post-feedback to not use libraries
 function camelCaseWords(words) {
   if (words === undefined) throw new Error('words is required');
-  const camelCase = require('camelcase');
-  return camelCase(words);
+  words = words.map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase());
+  words[0] = words[0].toLowerCase();
+  return words.join('');
 }
 
-// need to refactor
+// refactored post-feedback to not use libraries
 function getTotalSubjects(people) {
   if (people === undefined) throw new Error('people is required');
-  const lodash = require('lodash');
   const subjectsPerPerson = people.map((person) => person.subjects.length);
-  return lodash.sum(subjectsPerPerson);
+  return subjectsPerPerson.reduce((a, b) => a + b);
 }
 
 function checkIngredients(menu, ingredient) {
@@ -24,7 +25,6 @@ function checkIngredients(menu, ingredient) {
   return menu.some((i) => i.ingredients.includes(ingredient));
 }
 
-// need to refactor
 function duplicateNumbers(arr1, arr2) {
   if (arr1 === undefined) throw new Error('arr1 is required');
   if (arr2 === undefined) throw new Error('arr2 is required');
